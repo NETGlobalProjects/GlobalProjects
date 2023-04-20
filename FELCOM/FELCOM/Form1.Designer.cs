9701c9ea17a7b583a55df7c96178e05a07bb81de
sistemaserpglobal <sistemaserpglobal@gmail.com>
2023-04-20 09:04:10 -0600

    Modifiqué el label

--- FELCOM/FELCOM/Form1.Designer.cs
+++ FELCOM/FELCOM/Form1.Designer.cs
@@ -37,9 +37,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(62, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(64, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Hola mundo";
             // 
             // Form1
             // 
